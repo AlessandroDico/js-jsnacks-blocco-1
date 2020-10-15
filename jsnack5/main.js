b63e25ff608bdbaa72420cb97157db7931f07222
Ale <alessandrochb@gmail.com>
2020-10-15 17:14:08 +0200

    concluso esercizio 5

---
+++ jsnack5/main.js
@@ -0,0 +1,19 @@
+/*
+Stampa il cubo dei primi N numeri, dove N è un numero
+indicato dall’utente.
+*/
+
+
+// CHIEDO NUMERO ALL'UTENTE
+
+var user_number = parseInt(prompt('scrivi fino a che numero desideri elevare al cubo'));
+// console.log(user_number);
+
+var add = 0;
+// CREO UN CICLO DI FOR PER PRENDERE IL RANGE DA 0 A USER_NUMBER
+
+for (var i = 0; i < user_number; i++) {
+    // console.log(user_number);
+    add = [i] * [i] * [i];
+    console.log(add);
+}
