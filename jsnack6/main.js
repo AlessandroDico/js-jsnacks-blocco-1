525ab11b6526913b1c44cdd59dda1928519b1b5c
Ale <alessandrochb@gmail.com>
2020-10-15 17:41:36 +0200

    logica esercizio 6 completa__devo solo capire perchè max 1000 = 1024

---
+++ jsnack6/main.js
@@ -0,0 +1,12 @@
+// Stampa le potenze di 2 senza superare il numero 1000
+
+// IL RISULTATO DELLA POTENZA NON DEVE SUPERARE 1000
+
+var array = [0];
+var result = 0;
+
+for (var i = 0; i < array.length && result < 1000; i++) {
+    result = [i] * [i];
+    array.push(result);
+    console.log(result);
+}
