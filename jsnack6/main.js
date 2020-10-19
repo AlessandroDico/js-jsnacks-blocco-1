e017dbc65773d78c2ae2cff89a2afa4e780534b1
Ale <alessandrochb@gmail.com>
2020-10-19 14:06:09 +0200

    aggiunta soluzione con il while

--- jsnack6/main.js
+++ jsnack6/main.js
@@ -6,7 +6,20 @@
 
 var risultato;
 
-for (var i = 0; Math.pow(2, (1 + i)) < 1000; i++) {
-    risultato = Math.pow(2, (1 + i));
+for (var i = 1; Math.pow(2, i) < 1000; i++) {
+    risultato = Math.pow(2, i);
     console.log(risultato);
+}
+
+// SOLUZIONE CON IL WHILE
+
+var base = 2;
+var limite = 1000;
+var potenza = 1;
+var esponente = 1
+
+while (potenza < limite) {
+    console.log(potenza);
+    potenza = Math.pow(base, esponente);
+    esponente++;
 }
